e7c19ee349b7959c34d635adde45baf8160b2b90
= <=>
2023-05-17 22:52:23 +0700

    Add heap sort

---
+++ src/module-1/lesson-9/heapSort.test.ts
@@ -0,0 +1,13 @@
+import { sort } from './heapSort'
+
+describe('sort', () => {
+  test('should sort correctly', () => {
+    const comparator = (a: number, b: number) => a - b
+    const input = [10, 6, 3, 3, 1, 5, -2]
+    const output = [...input].sort(comparator)
+
+    sort(input, comparator)
+    
+    expect(input).toEqual(output)
+  })
+})
---
+++ src/module-1/lesson-9/heapSort.ts
@@ -0,0 +1,43 @@
+type TComparator<T> = (a: T, b: T) => number
+
+const heapify = <T>(list: T[], length: number, index: number, comparator: TComparator<T>) => {
+  const left = 2 * index + 1
+  const right = 2 * index + 2
+  let max = index
+
+  if (left < length && comparator(list[left], list[max]) > 0) {
+      max = left
+  }
+
+  if (right < length && comparator(list[right], list[max]) > 0)     {
+      max = right
+  }
+
+  if (max !== index) {
+      let temp = list[index]
+
+      list[index] = list[max]
+      list[max] = temp
+
+      heapify(list, length, max, comparator)
+  }
+}
+
+const buildHeap = <T>(list: T[], comparator: TComparator<T>) => {
+  for (let i = Math.floor((list.length - 1)  / 2); i >= 0; i -= 1) {
+    heapify(list, list.length, i, comparator)
+  }
+}
+
+export const sort = <T = number>(list: T[], comparator: TComparator<T>) => {
+  buildHeap(list, comparator)
+
+  for (let i = list.length - 1; i >= 0; i -= 1) {
+    let temp = list[i]
+
+    list[i] = list[0]
+    list[0] = temp
+
+    heapify(list, i, 0, comparator)
+  }
+}
